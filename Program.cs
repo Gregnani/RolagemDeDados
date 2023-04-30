385bca081654cdef118e96ba40f6484f2d57b893
gregnani <fgregnani@gmail.com>
2023-04-30 15:00:47 -0300

    atividade concluida

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿//fazer funcionar o random
+using System.Security.Cryptography;
+{
+        Console.WriteLine("---Rolagem de Dados---\n");
+
+        Console.Write("Quantos lados?");
+        int lados = Convert.ToInt32(Console.ReadLine());
+
+        int jogada = RandomNumberGenerator.GetInt32(1, lados + 1);
+
+        Console.WriteLine($"\nRolagem: {jogada}");
+}
